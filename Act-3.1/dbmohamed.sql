c21e611e2892589d6d92835491b4b8686a940b52
Mohamed Hossein Talha <mhtalha@dom.tti>
2022-06-22 11:35:33 +0100

    remplissage ft_table

--- Act-3.1/dbmohamed.sql
+++ Act-3.1/dbmohamed.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 22 juin 2022 à 12:14
+-- Généré le : mer. 22 juin 2022 à 12:33
 -- Version du serveur : 10.4.24-MariaDB
 -- Version de PHP : 7.4.29
 
@@ -653,6 +653,17 @@ CREATE TABLE `ft_table` (
   `date_de_creation` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Déchargement des données de la table `ft_table`
+--
+
+INSERT INTO `ft_table` (`id`, `login`, `groupe`, `date_de_creation`) VALUES
+(1, 'loki', 'staff', '2013-05-01'),
+(2, 'scadoux', 'student', '2014-01-01'),
+(3, 'chap', 'staff', '2011-04-27'),
+(4, 'bambou', 'staff', '2014-03-01'),
+(5, 'fantomet', 'staff', '2010-04-03');
+
 -- --------------------------------------------------------
 
 --
@@ -980,7 +991,7 @@ ALTER TABLE `salle`
 -- AUTO_INCREMENT pour la table `ft_table`
 --
 ALTER TABLE `ft_table`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
